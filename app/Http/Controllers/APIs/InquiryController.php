29a2987dd510c4819163396fd3dfc824eecf3568
ahmedansari6112 <ahmedansari6112@gmail.com>
2026-04-27 19:44:44 +0500

    new changes

--- app/Http/Controllers/APIs/InquiryController.php
+++ app/Http/Controllers/APIs/InquiryController.php
@@ -20,6 +20,7 @@ class InquiryController extends BaseApiController
     private const ADMIN_RECIPIENTS = [
         'tahreem@falcondrive.ae',
         'sales@falcondrive.ae',
+        'abbas@quickdigitals.ae'
     ];
 
     protected string $modelClass = Inquiry::class;
@@ -64,11 +65,11 @@ class InquiryController extends BaseApiController
             }
         }
 
-        // try {
-        //     Mail::to(self::ADMIN_RECIPIENTS)->send(new InquiryConfirmationMail($record, 'admin'));
-        // } catch (Throwable $mailException) {
-        //     report($mailException);
-        // }
+        try {
+            Mail::to(self::ADMIN_RECIPIENTS)->send(new InquiryConfirmationMail($record, 'admin'));
+        } catch (Throwable $mailException) {
+            report($mailException);
+        }
     }
 
     private function guardAgainstSpam(Request $request, array $data): void
