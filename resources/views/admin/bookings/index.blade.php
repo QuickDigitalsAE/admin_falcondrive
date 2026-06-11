1bfa88d04097df3bcfe64752856aeab9089627cf
ahmedansari6112 <ahmedansari6112@gmail.com>
2026-06-11 19:08:09 +0500

    Update final amount calculation to include discount and charges tax

--- resources/views/admin/bookings/index.blade.php
+++ resources/views/admin/bookings/index.blade.php
@@ -1770,7 +1770,9 @@
             const amountField = $('input[name="amount"]');
             const baseSubtotal = parseFloat(totalChargesField.data('baseSubtotal')) || 0;
             const baseAmount = parseFloat(amountField.data('baseAmount')) || 0;
-            const finalAmount = Math.round((baseAmount + (total - baseSubtotal)) * 100) / 100;
+            const discount = parseFloat($('input[name="discount"]').val()) || 0;
+            const chargesTax = parseFloat($('input[name="chargesTax"]').val()) || 0;
+            const finalAmount = Math.round((baseAmount + (total - baseSubtotal) - discount + chargesTax) * 100) / 100;
             $('input[name="amount"]').val(finalAmount);
         }
 
